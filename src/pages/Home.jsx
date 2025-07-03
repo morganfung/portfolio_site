7a11e3a5266c4931355e6eb63506ae24f63240b2
morganfung <morganfung2004@gmail.com>
2025-07-02 21:11:22 -0700

    Create Home and NotFound pages and simple logic

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+
+export const Home = () => {
+    return <div>
+        
+        {/* Theme Toggle: Deal with buttons on top right (dark/light mode) */}
+
+        {/* Background Effects: particles and meteor shower */}
+
+        {/* Navbar: self-explanatory */}
+
+        {/* Main Content: Hero, About, Skills, Projects */}
+
+        {/* Footer */}
+    </div>
+}
