7a12762916a53ea0f32905ccefa9b6ba3677b14c
morganfung <morganfung2004@gmail.com>
2025-07-10 21:21:02 -0700

    add stars, meteors, and basic navbar

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,26 +1,45 @@
 import { Moon, Sun } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { cn } from '@/lib/utils';
 
 export const ThemeToggle = () => {
     /* Create state variable so we can tell what mode (light/dark) the site is in. */
     /* Set true to  start in light mode. */
     const [isDarkMode, setIsDarkMode] = useState(false);
 
+    useEffect(() => {
+        const storedTheme = localStorage.getItem("theme");
+        if (storedTheme == "dark") {
+            setIsDarkMode(true);
+            document.documentElement.classList.add("dark");
+        } else {
+            setIsDarkMode(false);
+            document.documentElement.classList.add("light"); 
+        }
+    }, [])
+
     /* Toggle theme function: check if dark true -> logic, else ... */
     const toggleTheme = () => {
         if (isDarkMode) {
             document.documentElement.classList.remove("dark");
+            localStorage.setItem("theme", "light");
             setIsDarkMode(false);
         } else {
             document.documentElement.classList.add("dark");
+            localStorage.setItem("theme", "dark");
             setIsDarkMode(true);
         }
     };
 
     /* isDarkMode true? if so set button to sun, else ... */
     return (
-        <button onClick={toggleTheme}>
-            {" "}
+        <button 
+            onClick={toggleTheme}
+            className={cn(
+                "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
+                "focus:outline-hidden"
+            )}
+        >
             {isDarkMode ? (
                 <Sun className="h-6 w-6 text-yellow-300" />
             ) : (
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,18 @@
+import { Navbar } from "../components/Navbar";
 import { ThemeToggle } from "../components/ThemeToggle"
+import { StarBackground } from '@/components/StarBackground';
 
+/* DELETE: VID STOPPED AT 41:00 MARK */
 export const Home = () => {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
         
         {/* Theme Toggle: Deal with buttons on top right (dark/light mode) */}
-            <ThemeToggle/>
+        <ThemeToggle />
         {/* Background Effects: particles and meteor shower */}
+        <StarBackground />
 
         {/* Navbar: self-explanatory */}
-
+        <Navbar />
         {/* Main Content: Hero, About, Skills, Projects */}
 
         {/* Footer */}
