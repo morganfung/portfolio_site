947737c7cef455c77a2b0d03c8b4bd4baf2aa99b
morganfung <morganfung2004@gmail.com>
2025-07-10 23:29:13 -0700

    projects section done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import { StarBackground } from '@/components/StarBackground';
 import { HeroSection } from '@/components/HeroSection';
 import { AboutSection } from '@/components/AboutSection';
 import { SkillsSection } from '@/components/SkillsSection';
+import { ProjectsSection } from '@/components/ProjectsSection';
 
 /* DELETE: VID STOPPED AT 41:00 MARK */
 export const Home = () => {
@@ -23,6 +24,7 @@ export const Home = () => {
             <HeroSection />
             <AboutSection />
             <SkillsSection />
+            <ProjectsSection />
         </main>
 
         {/* Footer */}
